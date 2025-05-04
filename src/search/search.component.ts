7c4a644d56aa2009fdf41c77cd0835e282055086
Craig Foote <CraigFoote@gmail.com>
2025-05-03 22:54:14 -0400

    tweak filter bookings

--- src/search/search.component.ts
+++ src/search/search.component.ts
@@ -4,17 +4,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
 import { MatTabsModule } from '@angular/material/tabs';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 import { PetService } from '../services/pet.service';
 import { Pet } from '../model/Pet';
+import { firstValueFrom } from 'rxjs';
 import { PetCardComponent } from '../pets/pet-card/pet-card.component';
 import { Router } from '@angular/router';
 import { Booking } from '../model/Booking';
 import { BookingCardComponent } from '../bookings/booking-card/booking-card.component';
+import { Gender } from '../model/Gender';
 
 @Component({
     selector: 'app-search',
@@ -40,18 +42,30 @@ import { BookingCardComponent } from '../bookings/booking-card/booking-card.comp
 })
 export class SearchComponent {
 
-    private filterBookings: boolean = true;
-
     @Input() pets: Pet[] = [];
     @Input() bookings: Booking[] = [];
     tempBookings: Booking[] = [];
 
+    /**
+     * Initializes the component by creating a temporary copy of the current bookings.
+     * This allows for any modifications to the bookings list without affecting the original data.
+     */
+    ngOnInit() {
+        this.tempBookings = [...this.bookings];
+    }
+
     constructor(private petService: PetService, private router: Router) { }
 
-    submitSearch(searchTerm: string) {
+    /**
+     * Performs a search on the database based on the given search term.
+     * Updates the component's properties with the search results.
+     * @param searchTerm - The search term to search for.
+     * @returns A promise that resolves to void when the search is complete.
+     */
+    async submitSearch(searchTerm: string): Promise<void> {
         const term = searchTerm.trim();
         if (term.length > 0) {
-            this.search(term);
+            await this.search(term);
         }
     }
 
@@ -63,67 +77,74 @@ export class SearchComponent {
         throw new Error('Method not implemented.');
     }
 
-    bookPet(pet: Pet) {
-        // Use router state to pass the complex Pet object
+    /**
+     * Navigates to the bookings page with the specified pet details.
+     * @param pet - The Pet object to be booked.
+     */
+    bookPet(pet: Pet): void {
         this.router.navigate(['/bookings'], { state: { petToBook: pet, mode: 'add', view: 'info' } });
     }
 
-    search(search: string) {
+    /**
+     * Performs a search on the database based on the given search term.
+     * Updates the component's properties with the search results.
+     * @param search - The search term to search for.
+     * @returns A promise that resolves to void when the search is complete.
+     */
+    async search(search: string): Promise<void> {
         this.pets = [];
         this.bookings = [];
-        this.petService.search(search).subscribe((value: any[]) => {
-            // value is an array of arrays
-            // each inner array contains objects of different types
-            // we need to check the type of each object and create the corresponding model
-            for (const innerList of value) {
-                for (const item of innerList) {
-                    attrs: for (const attr in item) { // 'in' iterates over the keys of the object 
-                        if (attr === 'gender') {
-                            // we have a Pet
-                            // resolve ids to objects
-                            const species = this.petService.getSpeciesById(item.speciesId).subscribe((species: any) => {
-                                const owner = this.petService.getOwnerById(item.ownerId).subscribe((owner: any) => {
-                                    const pet = new Pet(item.name, species, item.gender, item.birthDate, owner);
-                                    pet.id = item.id; // Assign the ID from the DTO
-                                    this.pets.push(pet);
-                                });
-                            });
-                            break attrs;
-                        } else if (attr === 'procedureId') {
-                            // we have a Booking
-                            // resolve ids to objects
-                            const pet = this.petService.getPetById(item.petId).subscribe((pet: any) => {
-                                const procedure = this.petService.getProcedureById(item.procedureId).subscribe((procedure: any) => {
-                                    const vet = this.petService.getVetById(item.vetId).subscribe((vet: any) => {
-                                        const booking = new Booking(new Date(item.date), pet, procedure, vet);
-                                        booking.id = item.id; // Assign the ID from the DTO
-                                        this.bookings.push(booking);
-                                    });
-                                });
-                            });
-                            break attrs;
-                        }
-                    }
+        const results = await firstValueFrom(this.petService.search(search));
+        for (const innerList of results) {
+            for (const item of innerList) {
+                if ('gender' in item) {
+                    // we have a Pet
+                    const species = await firstValueFrom(this.petService.getSpeciesById(item.speciesId));
+                    const owner = await firstValueFrom(this.petService.getOwnerById(item.ownerId));
+                    const pet = new Pet(item.name, species, item.gender as Gender, item.birthDate, owner);
+                    pet.id = item.id; // Assign the ID from the DTO
+                    this.pets.push(pet);
+                    this.pets.sort((a, b) => a.name.localeCompare(b.name));
+                } else if ('procedureId' in item) {
+                    // we have a Booking
+                    const pet = await firstValueFrom(this.petService.getPetById(item.petId));
+                    const procedure = await firstValueFrom(this.petService.getProcedureById(item.procedureId));
+                    const vet = await firstValueFrom(this.petService.getVetById(item.vetId));
+                    const booking = new Booking(new Date(item.date), pet, procedure, vet);
+                    booking.id = item.id; // Assign the ID from the DTO
+                    this.bookings.push(booking);
+                    this.bookings.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
                 }
-                this.pets.sort((a, b) => a.name.localeCompare(b.name));
-                this.bookings.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
             }
-        });
+        }
     }
 
-    toggleBookingsFilter() {
-        this.filterBookings = !this.filterBookings;
-        if (this.filterBookings) {
-            this.tempBookings = this.bookings;
-            this.bookings = this.bookings.filter(booking => {
-                const now = new Date();
-                now.setHours(0, 0, 0, 0);
-                const bookingDate = new Date(booking.date);
-                bookingDate.setHours(0, 0, 0, 0);
-                return bookingDate.getTime() >= now.getTime();
-            });
-        } else {
-            this.bookings = this.tempBookings;
+    /**
+     * Toggles the filter for bookings to either show or hide past bookings based on the checkbox state.
+     * When the checkbox is checked, it filters the list of bookings to only include those with dates
+     * equal to or later than the current date. When unchecked, it restores the original list of bookings.
+     * The temporary state of the bookings is stored in `tempBookings` for restoration when the filter is removed.
+     *
+     * @param $event - The checkbox change event containing the checked state.
+     * @returns A promise that resolves to void when the filtering is complete.
+     */
+    async toggleBookingsFilter($event: MatCheckboxChange): Promise<void> {
+        const isChecked = $event?.checked;
+        if (isChecked === true) {
+            const now = new Date();
+            now.setHours(0, 0, 0, 0);
+
+            if (this.bookings) {
+                this.tempBookings = [...this.bookings];
+                this.bookings = this.bookings.filter((booking: Booking) => {
+                    const bookingDate = new Date(booking?.date);
+                    bookingDate.setHours(0, 0, 0, 0);
+                    return bookingDate?.getTime() >= now.getTime();
+                });
+                this.bookings = this.bookings.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+            }
+        } else if (this.tempBookings) {
+            this.bookings = [...this.tempBookings];
         }
     }
 }
