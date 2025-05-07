aefb24ced54535fdba56a2a7cafc2c2cc657cb91
Craig Foote <CraigFoote@gmail.com>
2025-05-07 11:38:26 -0400

    added auth to service calls

--- src/services/pet.service.ts
+++ src/services/pet.service.ts
@@ -14,142 +14,174 @@ import { BookingDTO } from "../model/BookingDTO";
 export class PetService {
 
     private host: string = 'http://localhost:10000';
+    private auth: string = 'Basic Y3JhaWc6cGV0cw==';
 
     constructor(@Inject(HttpClient) private http: HttpClient) { }
 
     search(term: string): Observable<any[]> {
-        return this.http.get<any>(this.host + '/search/' + term);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any>(this.host + '/search/' + term, { headers });
     }
 
     // VETS
 
     getVetById(id: string): Observable<any> {
-        return this.http.get<any>(this.host + '/vets/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any>(this.host + '/vets/' + id, { headers });
     }
 
     getAllVets(): Observable<any[]> {
-        return this.http.get<any[]>(this.host + '/vets');
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any[]>(this.host + '/vets', { headers });
     }
 
     insertVet(vet: Vet): Observable<any> {
-        return this.http.post<any>(this.host + '/vets', vet);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.post<any>(this.host + '/vets', vet, { headers });
     }
 
     deleteVet(id: string) {
-        return this.http.delete<any>(this.host + '/vets/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.delete<any>(this.host + '/vets/' + id, { headers });
     }
 
     updateVet(id: string, vet: Vet): Observable<any> {
-        return this.http.put<any>(this.host + '/vets/' + id, vet);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.put<any>(this.host + '/vets/' + id, vet, { headers });
     }
 
     // SPECIES
 
     getSpeciesById(id: string): Observable<any> {
-        return this.http.get<any>(this.host + '/species/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any>(this.host + '/species/' + id, { headers });
     }
 
     getAllSpecies(): Observable<any[]> {
-        return this.http.get<any[]>(this.host + '/species');
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any[]>(this.host + '/species', { headers });
     }
 
     insertSpecies(species: Species): Observable<any> {
-        return this.http.post<any>(this.host + '/species', species);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.post<any>(this.host + '/species', species, { headers });
     }
 
     deleteSpecies(id: string) {
-        return this.http.delete<any>(this.host + '/species/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.delete<any>(this.host + '/species/' + id, { headers });
     }
 
     updateSpecies(id: string, species: Species): Observable<any> {
-        return this.http.put<any>(this.host + '/species/' + id, species);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.put<any>(this.host + '/species/' + id, species, { headers });
     }
 
     // PROCEDURES
 
     getProcedureById(id: string): Observable<any> {
-        return this.http.get<any>(this.host + '/procedures/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any>(this.host + '/procedures/' + id, { headers });
     }
 
     getAllProcedures(): Observable<any[]> {
-        return this.http.get<any[]>(this.host + '/procedures');
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any[]>(this.host + '/procedures', { headers });
     }
 
     insertProcedure(procedure: Procedure): Observable<any> {
-        return this.http.post<any>(this.host + '/procedures', procedure);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.post<any>(this.host + '/procedures', procedure, { headers });
     }
 
     deleteProcedure(id: string) {
-        return this.http.delete<any>(this.host + '/procedures/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.delete<any>(this.host + '/procedures/' + id, { headers });
     }
 
     updateProcedure(id: string, procedure: Procedure): Observable<any> {
-        return this.http.put<any>(this.host + '/procedures/' + id, procedure);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.put<any>(this.host + '/procedures/' + id, procedure, { headers });
     }
 
     // PETS
 
     getPetById(id: string): Observable<any> {
-        return this.http.get<any>(this.host + '/pets/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any>(this.host + '/pets/' + id, { headers });
     }
 
     getAllPets(): Observable<any[]> {
-        return this.http.get<any[]>(this.host + '/pets');
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any[]>(this.host + '/pets', { headers });
     }
 
     insertPet(pet: PetDTO): Observable<any> {
-        return this.http.post<any>(this.host + '/pets', pet);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.post<any>(this.host + '/pets', pet, { headers });
     }
 
     deletePet(id: string) {
-        return this.http.delete<any>(this.host + '/pets/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.delete<any>(this.host + '/pets/' + id, { headers });
     }
 
     updatePet(id: string, pet: PetDTO): Observable<any> {
-        return this.http.put<any>(this.host + '/pets/' + id, pet);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.put<any>(this.host + '/pets/' + id, pet, { headers });
     }
 
     // OWNERS
 
     getOwnerById(id: string): Observable<any> {
-        return this.http.get<any>(this.host + '/owners/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any>(this.host + '/owners/' + id, { headers });
     }
 
     getAllOwners(): Observable<any[]> {
-        return this.http.get<any[]>(this.host + '/owners');
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any[]>(this.host + '/owners', { headers });
     }
 
     insertOwner(owner: Owner): Observable<any> {
-        return this.http.post<any>(this.host + '/owners', owner);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.post<any>(this.host + '/owners', owner, { headers });
     }
 
     deleteOwner(id: string) {
-        return this.http.delete<any>(this.host + '/owners/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.delete<any>(this.host + '/owners/' + id, { headers });
     }
 
     updateOwner(id: string, owner: Owner): Observable<any> {
-        return this.http.put<any>(this.host + '/owners/' + id, owner);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.put<any>(this.host + '/owners/' + id, owner, { headers });
     }
 
     // BOOKINGS
 
     getBookingById(id: string): Observable<any> {
-        return this.http.get<any>(this.host + '/bookings/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any>(this.host + '/bookings/' + id, { headers });
     }
 
     getAllBookings(): Observable<any[]> {
-        return this.http.get<any[]>(this.host + '/bookings');
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.get<any[]>(this.host + '/bookings', { headers });
     }
 
     insertBooking(booking: BookingDTO): Observable<any> {
-        return this.http.post<any>(this.host + '/bookings', booking);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.post<any>(this.host + '/bookings', booking, { headers });
     }
 
     deleteBooking(id: string) {
-        return this.http.delete<any>(this.host + '/bookings/' + id);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.delete<any>(this.host + '/bookings/' + id, { headers });
     }
 
     updateBooking(id: string, booking: BookingDTO): Observable<any> {
-        return this.http.put<any>(this.host + '/bookings/' + id, booking);
+        const headers = { 'Access-Control-Allow-Origin': '*', 'Authorization': this.auth };
+        return this.http.put<any>(this.host + '/bookings/' + id, booking, { headers });
     }
 }
